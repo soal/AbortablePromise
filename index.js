0941ec153c1893571647c97661a5fde4f48e2617
soal <sorrow.about.alice@pm.me>
2019-04-21 17:11:24 +0300

    Added actual code

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * Class for abortable promise.
+ *
+ * @class      AbortablePromise (name)
+ */
+class AbortablePromise {
+  constructor(executor, aborter = new AbortController()) {
+    this.abortController = aborter;
+    if (executor instanceof Promise) {
+      this.promise = executor;
+    } else {
+      this.promise = new Promise((resolve, reject) => executor(resolve, reject, this.abortController.signal));
+    }
+  }
+
+  then(...args) {
+    return new AbortablePromise(this.promise.then(...args), this.abortController);
+  }
+
+  catch(...args) {
+    return new AbortablePromise(this.promise.catch(...args), this.abortController);
+  }
+
+  finally(...args) {
+    return this.promise.finally(...args);
+  }
+
+  abort() {
+    return this.abortController.abort();
+  }
+}
+
+AbortablePromise.resolve = function(aborter = null, ...args) {
+  return new AbortablePromise(Promise.resolve(...args), aborter);
+};
+
+AbortablePromise.reject = function(aborter = null, err) {
+  return new AbortablePromise(Promise.reject(err), aborter);
+};
+
+AbortablePromise.race = function(aborter = null, ...args) {
+  return new AbortablePromise(Promise.race(...args), aborter);
+};
+
+AbortablePromise.all = function(aborter = null, ...args) {
+  return new AbortablePromise(Promise.all(...args), aborter);
+};
+
+export default AbortablePromise;
